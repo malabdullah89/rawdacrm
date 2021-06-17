d9586d18414140c9812c5233a39e7b4eae5f7e6d
malabdullah89 <53144490+malabdullah89@users.noreply.github.com>
2021-06-17 21:12:15 +0300

    mobile css

--- src/components/NewSub.js
+++ src/components/NewSub.js
@@ -4,7 +4,7 @@ import firebase from '../firebase'
 
 function NewSub() {
 
-    
+
 
     const [shareholders, setShareholders] = useState([]);
 
@@ -14,94 +14,102 @@ function NewSub() {
 
     const handleChange = (event) => {
         setTextInput(event.target.value);
-      }
+    }
 
 
-    
+
     function getShareholders() {
         ref.onSnapshot((querySnapshot) => {
             const items = [];
-            querySnapshot.forEach((doc) => { 
+            querySnapshot.forEach((doc) => {
                 items.push(doc.data());
                 console.log(doc.data())
             });
-            
+
             setShareholders(items);
         })
-        
+
     }
 
     useEffect(() => {
-        
+
         getShareholders();
 
     }, [])
 
-    
+
 
 
     return (
-        
+
         <Container>
             <h4>اشتراك جديد ( البرنامج الصيفي 2021 )</h4>
-             <form>
-             <h5>بيانات المساهم</h5>
-             <hr></hr>
-             <h6>الرقم المدني</h6> 
-             <input onChange={handleChange} placeholder="ادخل الرقم المدني"></input>
-            
-             </form>
-            
-           
+            <form>
+                <h5>بيانات المساهم</h5>
+                <h6>الرقم المدني</h6>
+                <input type="number" onChange={handleChange} placeholder="ادخل الرقم المدني"></input>
+
+            </form>
+
+
             {shareholders.map((shareholder) => {
-                if(shareholder.civilNumber === textInput) {
+                if (shareholder.civilNumber === textInput) {
                     return (
                         <div key={shareholder.civilNumber}>
-                         
-                         <form>
-                         <h6> الأسم</h6> 
-                         <Result readOnly="true" value={shareholder.shareholderName}></Result>
-                         <h6> رقم الصندوق</h6> 
-                         <Result readOnly="true" value={shareholder.shareholdingNum}></Result>
-                         <h6> رقم الهاتف</h6> 
-                         <Result placeholder="رقم الهاتف"></Result>
-                         <br></br>
-                         <hr></hr>
-                         <h6> الاشتراك</h6> 
-                         <ServicesMenu>
-                            <option value="volvo">يرجى اختيار الاشتراك ...</option>
-                            <option value="volvo">اشتراك اوكسجين 3 شهور</option>
-                            <option value="saab">اشتراك اوكسجين 6 شهور</option>
-                            <option value="opel">اشتراك اوكسجين سنة</option>
-                            <option value="audi">اشتراك bein sport</option>
-                         </ServicesMenu>
-                         </form>
 
-                         <br></br>
+                            <form>
+                                <h6> الأسم</h6>
+                                <Result readOnly="true"  value={shareholder.shareholderName}></Result>
+                                <h6> رقم الصندوق</h6>
+                                <Result readOnly="true" value={shareholder.shareholdingNum}></Result>
+                                <h6> رقم الهاتف</h6>
+                                <Result placeholder="رقم الهاتف"></Result>
+                                <br></br>
+                                <hr></hr>
+                                <h6> الاشتراك</h6>
+                                <ServicesMenu>
+                                    <option value="volvo">يرجى اختيار الاشتراك ...</option>
+                                    <option value="volvo">اشتراك اوكسجين 3 شهور</option>
+                                    <option value="saab">اشتراك اوكسجين 6 شهور</option>
+                                    <option value="opel">اشتراك اوكسجين سنة</option>
+                                    <option value="audi">اشتراك bein sport</option>
+                                </ServicesMenu>
+                            </form>
 
-                         <div>
-                             <hr></hr>
-                             <h5>وسيلة الدفع</h5> 
-                             <input type="radio" id="cash" name="payment"  value="cash"></input>
-                             <label for="male">نقدا</label><br></br>
-                             <input type="radio" id="knet" name="payment"  value="knet"></input>
-                             <label for="male">K-NET<span><img width="30px"  src="https://nalbes.com/assets/icons/Knet.png" /></span></label><br></br>
+                            <br></br>
 
-                             <hr></hr>
-                         </div>
+                            <div>
+                                <hr></hr>
+                                <h5>وسيلة الدفع</h5>
+                                
+                                <label for="male">نقدا
+                                <input type="radio" id="cash" name="payment" value="cash"></input>
+                                <span class="checkmark"></span>
 
-                         <button>تسجيل</button>
+                                </label>
+                                <br></br>
+                                
+                                <label for="male">K-NET<span><img width="30px" src="https://nalbes.com/assets/icons/Knet.png" /></span>
+                                <input type="radio" id="cash" name="payment" value="cash"></input>
+                                <span class="checkmark"></span>
+                                </label>
+                                <br></br>
+
+                                <hr></hr>
+                            </div>
+
+                            <button>تسجيل</button>
                         </div>
                     )
-                
-                } 
-               
+
+                }
+
             })}
 
-           
+
         </Container>
     )
-    
+
 }
 
 
@@ -114,10 +122,10 @@ const Container = styled.div`
   
     text-align: center; 
     margin-right: 20px;
-    width: 50vw;
-    padding: 50px 20px;
-    
+    width: 100vw;
+    padding: 50px 0px;
     margin: auto;
+    margin-top: 20px;
     border: none;
     border-radius: 50px;
     margin-bottom: 20px;
@@ -134,6 +142,14 @@ const Container = styled.div`
         background: #1b4285;
         color: white;
         font-size: 20px;
+        &:hover {
+
+            box-shadow: rgb(0 0 0 / 30%) 0px 40px 58px -16px,
+            rgb(0 0 0 / 30%) 0px 30px 22px -10px;
+            transform: scale(1.05);
+
+        }
+       
        
     }
 
@@ -152,14 +168,12 @@ const Container = styled.div`
             width: 300px;
             height: 45px;
             border: 1px solid #1b4285;
-            border-radius: 5px;
+            border-radius: 50px;
             color: #1b4285;
             text-align: center;
             font-size: 18px;
-            
-            
-
-
+            -moz-appearance: textfield;
+           
             &:focus {
                 outline: none;
                 border: 2px solid  #1b4285;
@@ -171,6 +185,17 @@ const Container = styled.div`
             }
 
         }
+
+        input::-webkit-outer-spin-button,
+        input::-webkit-inner-spin-button {
+                     -webkit-appearance: none;
+                        margin: 0;
+}
+
+
+input[type=number] {
+  -moz-appearance: textfield;
+}
 
         
 
@@ -191,7 +216,7 @@ const Result = styled.input`
             
             height: 45px;
             border: 1px solid #1b4285;
-            border-radius: 8px;
+            border-radius: 50px;
             color: #1b4285;
             text-align: center;
             font-size: 18px;
@@ -204,10 +229,18 @@ const ServicesMenu = styled.select`
             width: 350px;
             height: 45px;
             border: 1px solid #1b4285;
-            border-radius: 5px;
+            border-radius: 50px;
             color: #1b4285;
-            text-align: center;
             font-size: 18px;
+            -webkit-appearance: none;
+            padding: 0px 8px;
+
+
+            option {
+                
+                
+                
+            }
            
 
 
